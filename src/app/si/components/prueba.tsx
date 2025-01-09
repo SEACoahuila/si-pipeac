0a16a04dc1003da4fd84e597ef8e127eb0ee0bab
Rene Lazalde <rene.lazalde@gmail.com>
2025-01-09 10:29:26 -0600

    para trabajar en ramas aparte

---
+++ src/app/si/components/prueba.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Prueba = () => {
+  return (
+    <>
+
+<h1>xzzzcxqdads</h1>
+    </>
+  )
+}
--- src/app/si/page.tsx
+++ src/app/si/page.tsx
@@ -1,10 +1,13 @@
+import { Prueba } from "./components/prueba";
 
 export default function LandingPage() {
   return (
-    <div className="flex  justify-center items-center mt-10">
+    <div className="  justify-center items-center mt-10">
       <h1>Landing usuarios</h1> 
       
       <h2>Nombre usuario</h2>
+
+      <Prueba />
     </div>
   );
 }
