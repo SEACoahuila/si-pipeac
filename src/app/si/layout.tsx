953f8a75851b58645467253df3c15282f63db84a
ednamtz <edna.martinez@outlook.es>
2025-01-10 13:25:36 -0600

    Se hace login

--- src/app/si/layout.tsx
+++ src/app/si/layout.tsx
@@ -1,11 +1,14 @@
 import { Navbar } from "./components/navbar";
 
+
+
 export default function SiLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    
     <div className="">
       <div className=" bg-slate-100">
         {/* <Navbar /> */}
