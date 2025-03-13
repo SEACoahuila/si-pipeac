f878b313de418c57db2ed5657a000bae398bf13d
Rene Lazalde <rene.lazalde@gmail.com>
2025-03-13 10:54:30 -0600

    Se prueban variables de github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        {process.env.NEXT_PUBLIC_BASE_API}
       </main>
        
         
